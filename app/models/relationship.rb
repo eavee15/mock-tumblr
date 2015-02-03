d98ddba950793e9f8aefd9f863eebf65e02bb9ca
eavee15 <evillalona111@gmail.com>
2015-02-03 16:46:02 -0500

    Added content to relationship.rb model

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,2 +1,10 @@
 class Relationship < ActiveRecord::Base
+	belongs_to :leader, 
+		class_name: "User",
+		foreign_key: :leader_id
+
+	belongs_to :follower,
+		class_name: "User",
+		foreign_key: :follower_id
+end
 end
