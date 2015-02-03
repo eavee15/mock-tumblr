b31ed49e2059193e3407aaf7f1118b0a29f8ea3c
eavee15 <evillalona111@gmail.com>
2015-02-03 17:34:01 -0500

    Added content to post_controller.rb controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,66 @@
 class PostsController < ApplicationController
-  def index
+def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
+    current_user
   end
 
-  def edit
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = session[:user_id]
+    
+    if @post.save
+      flash[:notice] = "thanks for your post!"
+    else
+      flash[:alert] = "couldn't create post. please try again."      
+    end
+    
+    redirect_to "/posts/#{@post.id}"
   end
 
   def show
+    @post = Post.find(params[:id])
+    @current_user = User.find(session[:user_id])
+    @post_comments = @post.comments
+    # @post.comments is the same as Comment.where(post_id == post.id)
+    @comment = Comment.new
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+    @current_user = User.find(session[:user_id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    
+    if @post.update(post_params)
+      flash[:notice] = "post updated!"
+    else 
+      flash[:alert] = "couldn't update post. please try again."
+    end
+    
+    redirect_to "/posts/#{params[:id]}"
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    
+    if @post.destroy
+      flash[:notice] = "post deleted!"
+    else
+      flash[:alert] = "couldn't delete post. please try again."
+    end
+
+    redirect_to "/"
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,65 @@
 class UsersController < ApplicationController
-  def index
+ def new
+   @user = User.new
   end
 
-  def edit
+  def create
+    @user = User.new(user_params)
+    if @user.save!
+      @follow = Follower.new(follower_id: @user.id, leader_id: @user.id)
+      @follow.save
+      session[:user_id] = @user.id
+      redirect_to "/home/#{@user.id}/dash"
+    else 
+      flash[:alert] = @user.errors.full_messages
+      redirect_to :back
+    end
   end
 
   def show
+    current_user
+    @user = User.find(params[:id])
+    @post = Post.new
+    @posts = Post.where(user_id: params[:id])
+    @follow = Follower.where(follower_id: @current_user.id, leader_id: params[:id]).first
+    @followers = @user.followers.all.order(:username)
+    flash[:delete] = "post deleted"
   end
+
+  def edit
+    current_user
+    @user = User.find(params[:id])
+  end
+
+  def update
+    current_user
+    @user = User.find(params[:id])
+    if @current_user.id == @user.id
+      @user.update(user_params)
+      flash[:notice] = "updates saved!"
+    else
+      flash[:alert] = "updates not saved; please try again."
+    end
+    redirect_to :back
+  end
+
+  def destroy
+    current_user
+    @user = User.find(params[:id])
+    if @current_user.id == @user.id
+      @user.destroy
+    else
+      flash[:alert] = "you are not given permission to destroy this user."
+      redirect_to :back
+    end
+    redirect_to '/'
+  end
+
+  private
+
+  
+  def user_params
+    params.require(:user).permit(:email, :password, :username, :title, :aboutme)
+  end
+
 end
