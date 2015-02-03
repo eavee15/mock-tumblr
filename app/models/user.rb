6d98ecdd95a5f7efd3e9290862b1cdf06a804761
eavee15 <evillalona111@gmail.com>
2015-02-03 16:17:26 -0500

    Added content user.rb model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,29 @@
 class User < ActiveRecord::Base
+	validates :password, :email, :username, presence: true
+	validates :password, confirmation: true
+	validates :password, length: {minimum: 6}
+	validates :email, uniqueness: true
+
+	has_many :posts, dependent: :destroy
+	has_many :comments, through: :posts, source: :comments, dependent: :destroy
+
+	has_many :relationships, 
+		class_name: "Follower",
+		foreign_key: :follower_id,
+		dependent: :destroy
+
+	has_many :leaderships,
+		class_name: "Follower",
+		foreign_key: :leader_id,
+		dependent: :destroy
+
+	has_many :followers,
+		through: :leaderships,
+		source: :follower
+
+	has_many :leaders,
+		through: :relationships,
+		source: :leader
+		
+end
 end
