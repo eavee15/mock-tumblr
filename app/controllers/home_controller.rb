3633202eb4c64aa15b838dd271eaeffa4da593e4
eavee15 <evillalona111@gmail.com>
2015-02-03 22:22:59 -0500

    Added content to home_controller.rb controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
-  def dashboard
+   def dashboard
+    @posts = Post.all
+    
+    if current_user
+        render :dashboard
+    else
+        render :signin
+    end
+
   end
 
   def signin
