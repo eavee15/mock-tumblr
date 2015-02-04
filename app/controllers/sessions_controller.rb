a3a6db14e76643cbcfd828a6e21452aa3d32ec72
eavee15 <evillalona111@gmail.com>
2015-02-03 22:20:46 -0500

    Added content to sessions_controller.rb controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,30 @@
 class SessionsController < ApplicationController
-  def new
+ 
+ def new	
   end
+
+  def create
+  	@user = User.where(email: user_params[:email]).first
+
+  	if @user && @user.password == user_params[:password]
+  		session[:user_id] = @user.id
+  		flash[:notice] = "Succesfully logged in"
+    else
+    	flash[:alert] = "Login failed, please try again"
+    end
+
+    redirect_to '/'
+  end
+
+  def destroy
+  	session[:user_id] = nil
+  	redirect_to '/'
+  end
+
+  private
+  
+  def user_params
+    params.require(:user).permit(:email, :password, :name)
+  end
+
 end
